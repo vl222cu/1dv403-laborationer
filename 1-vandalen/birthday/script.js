e2cfd9ae717923afa9a82ed10d2624691b1fb060
OpenShift guest <vl222cu@student.lnu.se>
2013-11-17 09:26:52 -0500

    korrigerat undantag

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -5,11 +5,12 @@ window.onload = function(){
 	
 	var birthday = function(date){
 	// Vid felaktig inmatning av datum kastas ett undantag med ett meddelande till användaren.
-    if (date != date.setFullYear) {
+    var validformat=/^\d{4}\-\d{1,2}\-\d{1,2}$/;    
+    if(!date.match(validformat)) {
 	throw new Error ("Oops! Din inmatning överrenstämmer inte med den rätta formen ÅÅÅÅ-MM-DD, försök igen!"); 
 	}                           
-			// Din kod här.
-
+	return "You good!";		// Din kod här.
+    
 
 
 
