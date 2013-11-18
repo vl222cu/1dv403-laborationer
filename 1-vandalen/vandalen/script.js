7d33d38a34795799452b08a5e1fd48044ee9a28c
OpenShift guest <vl222cu@student.lnu.se>
2013-11-18 18:04:00 -0500

    sorterat maxAge och beräknat av.Age

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,16 +1,30 @@
 "use strict";
 
     var makePerson = function(persArr){
+    var length = persArr.length;
+    var sum =  0;
+    
+    for (var i = 0; i < length; ++i) {
+        sum += persArr[i].age;
+    }
+    var avAge = Math.round(sum / length);
     
     persArr.sort(function(a, b){
     return a.age-b.age;
     });
     var minAge = persArr[0].age;
     
+    persArr.sort(function(a, b){
+    return b.age-a.age;
+    });
+    var maxAge = persArr[0].age;
+    
+
 	// Din kod här...
-	var result = {minAge: minAge};
+	var result = {minAge: minAge, maxAge: maxAge, averageAge : avAge,};
 	return result;
     };
+    
 
     var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
 
