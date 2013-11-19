593a07bd8564a6a1e54e2158e9488ac1d4e5da34
OpenShift guest <vl222cu@student.lnu.se>
2013-11-19 10:57:49 -0500

    lagt till kommentarer

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -3,34 +3,34 @@
     var makePerson = function(persArr){
     var length = persArr.length;
     var sum =  0;
-    
+    // Uträkning av medelåldern
     for (var i = 0; i < length; ++i) {
         sum += persArr[i].age;
     }
     var avAge = Math.round(sum / length);
-    
+    // Sorterar för att få fram minimumåldern
     persArr.sort(function(a, b){
     return a.age-b.age;
     });
     var minAge = persArr[0].age;
-    
+    // Sorterar för att få fram maxåldern
     persArr.sort(function(a, b){
     return b.age-a.age;
     });
     var maxAge = persArr[0].age;
-    
+    // Sorterar namnen i bokstavsordning
     persArr.sort(function(a, b){
     var nameA = a.name.toLowerCase().localeCompare(b.name.toLowerCase());
     return nameA ? nameA : a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
     });
+    // Trimmar utskrift av namnen
     var allNames = "";
-    
     for (i = 0; i < length; ++i) {
         allNames += persArr[i].name + ", ";
     }
     allNames = allNames.trim();
     allNames = allNames.replace(/(^\s*,)|(,\s*$)/g, "");
-    
+    // Resultatet på värdena som ska skrivas ut
 	var result = {minAge: minAge, maxAge: maxAge, averageAge : avAge, names: allNames};
 	return result;
     };
