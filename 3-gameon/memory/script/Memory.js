4996f4c24af020378d092c53ae68f16f48d0f8e7
OpenShift guest <vl222cu@student.lnu.se>
2013-12-05 19:40:09 -0500

    skapat Memory-objekt

---
+++ 3-gameon/memory/script/Memory.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var Memory = {
+    
+    tiles: [],
+    
+    init : function() {
+        var date = new Date();
+        alert(date);
+    }
+
+};
+window.onload = Memory.init;
