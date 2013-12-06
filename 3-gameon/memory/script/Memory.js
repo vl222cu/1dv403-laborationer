9b39d54d47a99218b85ca918e71428c6d98ece28
OpenShift guest <vl222cu@student.lnu.se>
2013-12-06 10:18:53 -0500

    testat att skriva ut randomarrayen

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -5,8 +5,10 @@ var Memory = {
     tiles: [],
     
     init : function() {
-        var date = new Date();
-        alert(date);
+        var rows = 4;
+        var cols = 4;
+        Memory.tiles = RandomGenerator.getPictureArray(rows, cols);
+        alert(Memory.tiles);
     }
 
 };
