6c2ac4309da4e5fe6dddd2e4acbc7def72863d47
OpenShift guest <vl222cu@student.lnu.se>
2013-12-09 15:54:55 -0500

    lyckats få två bilder att öppna sig

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -1,33 +1,52 @@
 "use strict";
+var rows = 4;
+var cols = 4;
 
 var Memory = {
-    
+    // Egenskap för att spara resultatet från den utslumpade getPictureArrayen
     tiles: [],
+    // Egenskap för att hålla så att det endast går att vända två brickor
+    pairs: [],
     
     init : function() {
-        var rows = 4;
-        var cols = 4;
+        
+        var pic = 0;
         var table = document.getElementById("gametable");
         
+        // Anrop av arrayslumpsmetoden och sparar resultatet i egenskapen tiles
         Memory.tiles = RandomGenerator.getPictureArray(rows, cols);
-
+        
         // Skapar tabell med randomarrayen
-        for(var i = 0; i < rows; i++) {
-            var tr = document.createElement("tr");
-            for(var j = 0; j < cols; j++) {
+        for(var i = 0; i < rows; i++){
+            var tr = table.insertRow();
+            for(var j = 0; j < cols; j++){
+                var td = tr.insertCell();
+                
+                // Kapslar in varje bild i en a-länk
                 var image = document.createElement("img");
                 var a = document.createElement("a");
-                a.className = "imgID";
+                a.id = "startimg";
                 image.setAttribute("src", "../pics/0.png");
                 image.alt = "hidden";
-                var td = document.createElement("td");
                 a.appendChild(image);
                 td.appendChild(a);
-                tr.appendChild(td);
+                
+                // Anropar onclickmetoden
+                Memory.flipTile(pic, a);
             }
-            table.appendChild(tr);
         }
-    }
-};
+    },   
+    
+    flipTile: function(pic, a) {
+        a.onclick = function() {
+        
+        Memory.pairs.push(a);
+        if (Memory.pairs.length < 3) {
+            this.getElementsByTagName("img")[0].setAttribute("src", "../pics/" + Memory.tiles[pic] + ".png");
+        } 
+        
+    };
+}
 
+};
 window.onload = Memory.init;
