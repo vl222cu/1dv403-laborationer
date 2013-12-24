0ec3e1896bb20d6e487bf85eb77bf8f64eeb020b
OpenShift guest <vl222cu@student.lnu.se>
2013-12-24 16:19:18 -0500

    fixat layout på fönstret

--- 5-pwd/window.js
+++ 5-pwd/window.js
@@ -10,11 +10,38 @@ VIWD.Window = function(width, height) {
 };
 
 VIWD.Window.prototype.openWindow = function() {
-     this.element = document.createElement("div");
-     this.element.id = "newwindow";
-     this.element.style.width = this.width + "px";
-     this.element.style.height = this.height + "px";
-     document.body.appendChild(this.element);
-
     
+    // Skapar tom objekt för fönsterelementen
+    this.element = {};
+    var e = this.element;
+    
+    // Parent div
+     e.maincontainer = document.createElement("div");
+     e.maincontainer.id = "newwindow";
+     e.maincontainer.style.width = this.width + "px";
+     e.maincontainer.style.height = this.height + "px";
+     
+     document.body.appendChild(e.maincontainer);
+     
+     // Plats för fönstrets namn
+     e.titlebar = document.createElement("div");
+     e.titlebar.className = "nwtitlebar";
+     
+     // Namn på fönstret
+     e.titlename = document.createElement("span");
+     e.titlename.className = "nwtitlename";
+     
+     e.maincontainer.appendChild(e.titlebar);
+     
+     // Fönstrets innehåll
+     e.content = document.createElement("div");
+     e.content.className = "nwcontent";
+     
+     e.maincontainer.appendChild(e.content);
+     
+     // Fönstrets footer
+     e.footer = document.createElement("div");
+     e.footer.className = "nwfooter";
+     
+     e.maincontainer.appendChild(e.footer);
 };
