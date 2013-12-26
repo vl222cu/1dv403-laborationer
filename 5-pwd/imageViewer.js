d6fa2651843fb8370079bbd34b21224c099a2ce0
OpenShift guest <vl222cu@student.lnu.se>
2013-12-26 13:22:53 -0500

    får inte Ajaxanropet att fungera :(

--- 5-pwd/imageViewer.js
+++ 5-pwd/imageViewer.js
@@ -2,13 +2,21 @@ var VIWD = VIWD || {};
 
 VIWD.ImageViewer = function () {
     // Anropar superklassens konstruktor
-    VIWD.Window.call(this, 450, 600, "Testar Viewer", "pics/pic.png");
+    VIWD.Window.call(this, 450, 600, "Image Viewer", "pics/pic.png");
+    VIWD.ImageViewer.prototype.getThumbPics();
 };
 
 // Ser till att ImageViewer ärver från superklassen Windows
 VIWD.ImageViewer.prototype = Object.create(VIWD.Window.prototype);
 
-
+// Ajaxanrop som kommer att returnera JSON-sträng med tumnagelbilder 
+VIWD.ImageViewer.prototype.getThumbPics = function () {
+    var url = "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/";
+    var container = document.getElementsByClassName("nwcontent");
+    new VIWD.AjaxCon(url, function(data) {
+        container.innerHTML = data;
+    });
+};
 
 
 
