fd7c9d0d67cac62a3f96fadfa7110293c6ef1a25
OpenShift guest <vl222cu@student.lnu.se>
2014-01-01 16:10:40 -0500

    rss-feed fungerar men ajaxloaderikonen vill inte försvinna

--- 5-pwd/rssReader.js
+++ 5-pwd/rssReader.js
@@ -1,9 +1,26 @@
 var VIWD = VIWD || {};
 
 VIWD.RssReader = function () {
+    "use strict";
     // Anropar superklassens konstruktor
-    VIWD.Window.call(this, 350, 500, "RSS Reader", "pics/rss.png");
+    VIWD.Window.call(this, 400, 500, "RSS Reader", "pics/rss.png");
+    VIWD.RssReader.prototype.displayFeed();
 };
 
 // Ser till att rssReader ärver från superklassen Window
 VIWD.RssReader.prototype = Object.create(VIWD.Window.prototype);
+
+VIWD.RssReader.prototype.displayFeed = function () {
+    "use strict";
+    $(document).ready(function () {
+        setTimeout(function () { $('#ajaxloader').show(); }, 300);
+        $.ajax({
+            url: "http://homepage.lnu.se/staff/tstjo/labbyServer/rssproxy/?url=" + encodeURI("http://www.dn.se/m/rss/senaste-nytt")
+        }).done(function (data) {
+           $('#ajaxloader').hide();
+           $('#nwcon').html(data);
+        }).fail(function (jqXHR, textStatus) {
+            console.log("Läsfel, status: " + textStatus);
+        }); 
+    });
+};
